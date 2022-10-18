4f8c415b027eedb2acdac496e3a8eddff03590c9
Seti Ngabo <seti.ngabo@mths.ca>
2022-10-18 13:53:12 +0000

    init

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,16 @@ import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
 
-const light = 2.998 * 10 ** 8
-
 // input and output
 const userString = prompt('What is the mass of the object?(kg) : ')
 const user = parseFloat(userString)
 
 if (user < 0) {
-  console.log('Negative number, tyry again.')
+  console.log('Negative number, try again.')
 } else if (user! > 0) {
   console.log(`${user} is not a number`)
 
-  const answer = (user * light) ** 2
+  const answer = user * (2.998 * 10 ** 8) ** 2
   console.log(`${answer} Joules of energy would be released`)
 } else {
   console.log('Invalid input, try again.')
